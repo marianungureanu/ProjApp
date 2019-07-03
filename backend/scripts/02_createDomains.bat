f4e3fce07126c304a4e4dad0e37444b298ac1394
Aurel <aurel.burja.bp@nttdata.ro>
2019-07-03 20:24:52 +0300

    scripts update

--- backend/scripts/02_createDomains.bat
+++ backend/scripts/02_createDomains.bat
@@ -1,33 +1,48 @@
 @echo off
 
-set GF_DIR=D:\practicaDeVara\glassfish4.1.2
+set GF_DIR=D:\practicaDeVara2019\glassfish4.1.2
 
 set FRONT_DOMAIN=projapp_front
-set /a FRONT_PORTBASE=1000
+set /a FRONT_PORTBASE=5000
 set /a FRONT_ADMINPORT=%FRONT_PORTBASE% + 48
 
 set BACK_DOMAIN=projapp_back
-set /a BACK_PORTBASE=2000
+set /a BACK_PORTBASE=6000
 set /a BACK_ADMINPORT=%BACK_PORTBASE% + 48
 set MYSQL_USER=projapp
 set MYSQL_PASS=projappPass
 set MYSQL_DB=projappdb
 
-@echo on
+rem https://bugs.eclipse.org/bugs/show_bug.cgi?id=463169
+xcopy org.eclipse.persistence.moxy-2.6.1.jar %GF_DIR%\glassfish\modules\
+if not exist %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy-2.6.1.jar (
+    echo "   Error. Cannot copy eclipse moxy jar. Eclipse moxy has to be copied to %GF_DIR%\glassfish\modules\"
+	pause
+	exit /b
+)
+
+rm %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar
+if exist %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar (
+    echo "   Error. org.eclipse.persistence.moxy.jar needs to be deleted from %GF_DIR%\glassfish\modules\"
+	pause
+	exit /b
+)
+
 echo Creating %FRONT_DOMAIN%
-rem @call %GF_DIR%\bin\asadmin.bat create-domain --portbase %FRONT_PORTBASE% %FRONT_DOMAIN%
+@call %GF_DIR%\bin\asadmin.bat create-domain --portbase %FRONT_PORTBASE% %FRONT_DOMAIN%
 echo Creating %BACK_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat create-domain --portbase %BACK_PORTBASE% %BACK_DOMAIN%
 xcopy mysql-connector-java-8.0.11.jar  %GF_DIR%\glassfish\domains\%BACK_DOMAIN%\lib\ext\
 if not exist %GF_DIR%\glassfish\domains\%BACK_DOMAIN%\lib\ext\mysql-connector-java*.jar (
-    echo "   Error. Mysql connector has to be copied to %BACK_DOMAIN%\lib\ext\"
+    echo "   Error. Cannot find mysqlconnector. Mysql connector has to be copied to %BACK_DOMAIN%\lib\ext\"
+	pause
 	exit /b
 )
 
 @call %GF_DIR%\bin\asadmin.bat start-domain %BACK_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat --port %BACK_ADMINPORT% --user admin create-jdbc-connection-pool ^
---driverclassname=com.mysql.cj.jdbc.Driver ^
---restype java.sql.Driver ^
+--datasourceclassname com.mysql.cj.jdbc.MysqlDataSource ^
+--restype javax.sql.DataSource ^
 --steadypoolsize 18  ^
 --maxpoolsize 32  ^
 --maxwait 60000 ^
@@ -39,7 +54,7 @@ if not exist %GF_DIR%\glassfish\domains\%BACK_DOMAIN%\lib\ext\mysql-connector-ja
 --validationmethod auto-commit  ^
 --failconnection=true  ^
 --description projappConnectionPool  ^
---property user=%MYSQL_USER%:password=%MYSQL_PASS%:URL=jdbc\:mysql\://localhost\:3306/%MYSQL_DB%:useSSL=false ^
+--property user=%MYSQL_USER%:password=%MYSQL_PASS%:URL=jdbc\:mysql\://localhost\:3306/%MYSQL_DB%:useSSL=true:serverTimezone=UTC ^
 projappJdbcPool 
 
 @call %GF_DIR%\bin\asadmin.bat --port %BACK_ADMINPORT% --user admin  create-jdbc-resource  ^
@@ -51,3 +66,5 @@ projappJdbcResource
 
 @call %GF_DIR%\bin\asadmin.bat stop-domain %BACK_DOMAIN%
 pause
+
+
