ef1512a5f19cfe062d26fe356c5a84f1100a9c89
Ovidiu Hulea <o_hulea@yahoo.com>
2019-07-04 13:36:47 +0300

    modificat

--- backend/scripts/01_setupMysql.bat
+++ backend/scripts/01_setupMysql.bat
@@ -1,4 +1,4 @@
 set mysql="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql"
-%mysql% --user=root   --password=root < mysqlCreateUser.sql
-%mysql% --user=projapp --password=projappPass < mysqlSetupDb.sql
+%mysql% --user=root --password=Spartacus16 "<" mysqlCreateUser.sql
+%mysql% --user=projapp --password=projappPass "<" mysqlSetupDb.sql
 pause
