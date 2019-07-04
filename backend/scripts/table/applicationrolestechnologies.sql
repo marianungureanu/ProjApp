a42df3af5b66b715177fe2f9150e38a0dc378aa4
Ovidiu Hulea <o_hulea@yahoo.com>
2019-07-04 09:37:14 +0300

    am creat tabela applicationrolestechnologies

--- backend/scripts/table/applicationrolestechnologies.sql
+++ backend/scripts/table/applicationrolestechnologies.sql
@@ -8,3 +8,16 @@
  * Created: Jul 3, 2019
  */
 
+CREATE TABLE `applicationrolestechnologies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idApplicationRole` int(11) NOT NULL,
+  `idTechnology` int(11) NOT NULL,
+  `idLevelMin` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idApplicationRole_idx` (`idApplicationRole`),
+  KEY `idTechnology_idx` (`idTechnology`),
+  KEY `idLevelMin_idx` (`idLevelMin`),
+  CONSTRAINT `idApplicationRoleTechnologies` FOREIGN KEY (`idApplicationRole`) REFERENCES `applicationroles` (`id`),
+  CONSTRAINT `idLevelMin` FOREIGN KEY (`idLevelMin`) REFERENCES `level` (`id`),
+  CONSTRAINT `idTechnologyRoleTechnologies` FOREIGN KEY (`idTechnology`) REFERENCES `technology` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
