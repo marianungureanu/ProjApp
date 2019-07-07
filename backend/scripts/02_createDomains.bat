5db5c0b912356b34c9cca3697b93ef5d9e8496b0
Marian Gheorghe Ungureanu <marian.ungureanu@nttdata.ro>
2019-07-07 18:51:14 +0300

    - modified 02_createDomains.bat file (there were some errors) - added docker-compose file for which want to use docker - added diagram for current database

--- backend/scripts/02_createDomains.bat
+++ backend/scripts/02_createDomains.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set GF_DIR=C:\glassfish4
+set set GF_DIR=C:\glassfish4
 
 set FRONT_DOMAIN=projapp_front
 set /a FRONT_PORTBASE=5000
@@ -21,18 +21,21 @@ if not exist %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy-2.6.1.jar (
 	exit /b
 )
 
-del %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar
+if exist %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar (
+	del %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar
+)
 if exist %GF_DIR%\glassfish\modules\org.eclipse.persistence.moxy.jar (
     echo "   Error. org.eclipse.persistence.moxy.jar needs to be deleted from %GF_DIR%\glassfish\modules\"
 	pause
 	exit /b
 )
 
-
 @call %GF_DIR%\bin\asadmin.bat delete-domain %FRONT_DOMAIN%
+@call %GF_DIR%\bin\asadmin.bat delete-domain %BACK_DOMAIN%
 
 echo Creating %FRONT_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat create-domain --portbase %FRONT_PORTBASE% %FRONT_DOMAIN%
+
 echo Creating %BACK_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat create-domain --portbase %BACK_PORTBASE% %BACK_DOMAIN%
 copy mysql-connector-java-8.0.11.jar  %GF_DIR%\glassfish\domains\%BACK_DOMAIN%\lib\ext\
@@ -42,7 +45,6 @@ if not exist %GF_DIR%\glassfish\domains\%BACK_DOMAIN%\lib\ext\mysql-connector-ja
 	exit /b
 )
 
-@call %GF_DIR%\bin\asadmin.bat delete-domain %BACK_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat start-domain %BACK_DOMAIN%
 @call %GF_DIR%\bin\asadmin.bat --port %BACK_ADMINPORT% --user admin create-jdbc-connection-pool ^
 --datasourceclassname com.mysql.cj.jdbc.MysqlDataSource ^
