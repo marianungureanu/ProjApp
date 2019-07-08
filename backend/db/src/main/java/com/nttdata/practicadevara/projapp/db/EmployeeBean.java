e3fb53e2376a5d251a535f003260e5967aab5ba7
Ovidiu Hulea <o_hulea@yahoo.com>
2019-07-08 13:17:09 +0300

    Get Employee by ID

--- backend/db/src/main/java/com/nttdata/practicadevara/projapp/db/EmployeeBean.java
+++ backend/db/src/main/java/com/nttdata/practicadevara/projapp/db/EmployeeBean.java
@@ -4,12 +4,13 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 @Stateless
 public class EmployeeBean {
 
     private static final long serialVersionUID = 117223295272084434L;
-
     public static final String SCHEMA_NAME = "projappdb";
 
     @PersistenceContext(unitName = "projapp-persistenceunit") //see main/resources/META-INF/peristence.xml
@@ -18,8 +19,19 @@ public class EmployeeBean {
     public List<EmployeeEntity> findAll() {
         return manager.createNamedQuery(findAllNamedQuery()).getResultList();
     }
+    
+    public EmployeeEntity findById(int id){
+        TypedQuery<EmployeeEntity> q = manager.createNamedQuery(findOneNamedQuery(), EmployeeEntity.class);
+        q.setParameter("id", id);
+        EmployeeEntity emplEntity = q.getSingleResult();
+        return emplEntity;
+    }
 
     public String findAllNamedQuery() {
         return EmployeeEntity.FIND_ALL;
+    }
+    
+    public String findOneNamedQuery(){
+        return EmployeeEntity.FIND_ONE;
     }
 }
