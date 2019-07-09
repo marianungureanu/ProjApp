61827f450db208ab24bf7de7e28a273adbd6b328
emilian.suroiu <emilian.suroiu@PC1702.ebs.msft>
2019-07-09 11:59:06 +0300

    GetRole

--- backend/scripts/01_setupMysql.bat
+++ backend/scripts/01_setupMysql.bat
@@ -1,4 +1,4 @@
 set mysql="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql"
-%mysql% -uroot -pYourPassword < .\mysqlCreateUser.sql
+%mysql% -uroot -p123456789 < .\mysqlCreateUser.sql
 %mysql% -uprojapp -pprojappPass < .\mysqlSetupDb.sql
 pause
