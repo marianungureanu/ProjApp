5ea5fad81b18ef7780dff4f3632182af7aced991
Luca Iuliana <iuliana.luca@nttdata.ro>
2019-07-10 10:23:09 +0300

    Afisare Technology

--- web/src/main/java/com/nttdata/practicadevara/projapp/front/technology/TechnologyRest.java
+++ web/src/main/java/com/nttdata/practicadevara/projapp/front/technology/TechnologyRest.java
@@ -1,15 +1,20 @@
 package com.nttdata.practicadevara.projapp.front.technology;
 
+import com.nttdata.practicadevara.projapp.front.RestClient;
 import com.nttdata.practicadevara.projapp.shared.dto.TechnologyDto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @LocalBean
-public class TechnologyRest {
+public class TechnologyRest extends RestClient {
 
     private int tempIndex = 0;           //to be delete when REST services are ready
     private List<TechnologyDto> tempList;  //to be delete when REST services are ready
@@ -17,10 +22,9 @@ public class TechnologyRest {
     @PostConstruct
     public void init() {
         tempList = new ArrayList<>();
-        for(int i=0; i<5; i++) { 
-            TechnologyDto e = new TechnologyDto(tempIndex++, "Technology " + tempIndex);
-            tempList.add(e);
-        }
+        
+        Response resp = super.path("technology").request(MediaType.APPLICATION_JSON).get(Response.class);
+        tempList.addAll(resp.readEntity(new GenericType<List<TechnologyDto>>(){}));
     }
 
     public List<TechnologyDto> listTechnologies() {
