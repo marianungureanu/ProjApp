d7272301e68bfdc77d66835c374111b1329ecda3
SireanuS <stefana.sireanu@nttdata.ro>
2019-07-16 16:15:24 +0300

    integrated BE with FE on subscription part

--- web/src/main/java/com/nttdata/practicadevara/projapp/front/subscription/SubscriptionRest.java
+++ web/src/main/java/com/nttdata/practicadevara/projapp/front/subscription/SubscriptionRest.java
@@ -5,27 +5,35 @@ package com.nttdata.practicadevara.projapp.front.subscription;
  * @author stefana.sireanu
  */
 
-import com.nttdata.practicadevara.projapp.shared.dto.ApplicationRoleDto;
-import com.nttdata.practicadevara.projapp.shared.dto.EmployeeDto;
-import com.nttdata.practicadevara.projapp.shared.dto.RoleDto;
+import com.nttdata.practicadevara.projapp.front.RestClient;
 import com.nttdata.practicadevara.projapp.shared.dto.SubscriptionDto;
-import com.nttdata.practicadevara.projapp.shared.dto.SubscriptionStatusEnumDto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @LocalBean
-public class SubscriptionRest {
+public class SubscriptionRest extends RestClient{
     
     private int tempIndex = 0;           //to be delete when REST services are ready
     private List<SubscriptionDto> tempList;  //to be delete when REST services are ready
 
     @PostConstruct
     public void init() {
-        int appRoleIdx = 1;
+     
+        tempList = new ArrayList<>();
+        Response resp= super.path("subscription").request(MediaType.APPLICATION_JSON).get(Response.class);
+      tempList.addAll(resp.readEntity(new GenericType<List<SubscriptionDto>>(){}));
+      
+      
+    
+        
+       /*  int appRoleIdx = 1;
         ApplicationRoleDto role1 = new ApplicationRoleDto(appRoleIdx++, new RoleDto(1, "Junior Dev") );
         ApplicationRoleDto role2 = new ApplicationRoleDto(appRoleIdx++, new RoleDto(2, "Middle Dev") );
         ApplicationRoleDto role3 = new ApplicationRoleDto(appRoleIdx++, new RoleDto(3, "Senior Dev") );
@@ -36,7 +44,7 @@ public class SubscriptionRest {
         tempList.add(new SubscriptionDto(3, SubscriptionStatusEnumDto.REJECTED, new EmployeeDto(2, "User B"), role2));
         tempList.add(new SubscriptionDto(4, SubscriptionStatusEnumDto.NEW,      new EmployeeDto(2, "User B"), role3));
         tempList.add(new SubscriptionDto(5, SubscriptionStatusEnumDto.ACCEPTED, new EmployeeDto(3, "User C"), role3));
-        tempList.add(new SubscriptionDto(6, SubscriptionStatusEnumDto.REJECTED, new EmployeeDto(3, "User C"), role2));
+        tempList.add(new SubscriptionDto(6, SubscriptionStatusEnumDto.REJECTED, new EmployeeDto(3, "User C"), role2));*/
     }
 
     public List<SubscriptionDto> listSubscriptions() {
